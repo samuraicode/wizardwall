722df9de735a1de2f516ab419620b5fa95673db3
Jeremy Noonan <jeremy@samuraicode.com>
2014-10-15 23:10:43 -0700

    Make sure to alpha the overlays

--- camManager.py
+++ camManager.py
@@ -52,23 +52,13 @@ class camManager(object):
 
 		# Load the arbitrarily sized image
 		img = Image.open(imagefile)
-		# Create an image padded to the required size with
-		# mode 'RGB'
 		pad = Image.new('RGB', (
 			((img.size[0] + 31) // 32) * 32,
 			((img.size[1] + 15) // 16) * 16,
 		))
-		# Paste the original image into the padded one
 		pad.paste(img, (0, 0))
-
-		# Add the overlay with the padded image as the source,
-		# but the original image's dimensions
 		o = self.camera.add_overlay(pad.tostring(), size=img.size)
-		# By default, the overlay is in layer 0, beneath the
-		# preview (which defaults to layer 2). Here we make
-		# the new overlay semi-transparent, then move it above
-		# the preview
-		# o.alpha = 128
+		o.alpha = 128
 		o.layer = 3
 
 	# Preview manipulation
